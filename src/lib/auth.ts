b3603c197cd37e05705088eafea13579eaa3c8bd
Melvunx <augustine.melv@gmmail.com>
2025-04-11 14:12:01 +0200

    setup prisma

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth from "next-auth";
 import authConfig from "./auth.config";
+import { prisma } from "./prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
-  providers: [],
+  adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
   },
