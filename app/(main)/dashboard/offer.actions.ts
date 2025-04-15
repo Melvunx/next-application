38d735a3d0f7cd5c5843eb94d0d5d2445dd71452
Melvunx <augustine.melv@gmmail.com>
2025-04-15 23:06:53 +0200

    update offer Actions

--- app/(main)/dashboard/offer.actions.ts
+++ app/(main)/dashboard/offer.actions.ts
@@ -1,4 +1,5 @@
 import { prisma } from "@/src/lib/prisma";
+import { OfferCreateSchema, OfferCreateType } from "@/src/schema/offer";
 import { z } from "zod";
 
 export const idValidateSchema = z.string().uuid().nonempty();
@@ -11,6 +12,99 @@ export const getOffersAction = async () => {
   } catch (error) {
     console.error("Error fetching offers:", error);
     throw new Error("Failed to fetch offers");
+  }
+};
+
+export const getOfferByIdAction = async (offerId: string) => {
+  try {
+    const parsedId = idValidateSchema.parse(offerId);
+
+    const offer = await prisma.offer.findUnique({ where: { id: parsedId } });
+
+    return offer;
+  } catch (error) {
+    console.error("Error fetching offer by ID:", error);
+    throw new Error("Failed to fetch offer by ID");
+  }
+};
+
+export const createOfferAction = async (offerData: OfferCreateType) => {
+  try {
+    const parsedData = OfferCreateSchema.parse(offerData);
+
+    const offer = await prisma.offer.create({ data: parsedData });
+
+    return offer;
+  } catch (error) {
+    console.error("Error creating offer:", error);
+    throw new Error("Failed to create offer");
+  }
+};
+
+export const updateOfferAction = async (
+  offerId: string,
+  offerData: OfferCreateType
+) => {
+  try {
+    const parsedId = idValidateSchema.parse(offerId);
+
+    const parsedData = OfferCreateSchema.parse(offerData);
+
+    const updatedOffer = await prisma.offer.update({
+      where: { id: parsedId },
+      data: parsedData,
+    });
+
+    return updatedOffer;
+  } catch (error) {
+    console.error("Error updating offer:", error);
+    throw new Error("Failed to update offer");
+  }
+};
+
+export const updateIsArchivedOfferAction = async (
+  offerId: string,
+  isArchived: boolean
+) => {
+  try {
+    const parsedId = idValidateSchema.parse(offerId);
+
+    const updatedArchive = await prisma.offer.update({
+      where: { id: parsedId },
+      data: { isArchived },
+      select: {
+        id: true,
+        isArchived: true,
+      },
+    });
+
+    return updatedArchive;
+  } catch (error) {
+    console.error("Error updating offer:", error);
+    throw new Error("Failed to update offer");
+  }
+};
+
+export const updateIsFavoriteOfferAction = async (
+  offerId: string,
+  isFavorite: boolean
+) => {
+  try {
+    const parsedId = idValidateSchema.parse(offerId);
+
+    const updatedArchive = await prisma.offer.update({
+      where: { id: parsedId },
+      data: { isFavorite },
+      select: {
+        id: true,
+        isFavorite: true,
+      },
+    });
+
+    return updatedArchive;
+  } catch (error) {
+    console.error("Error updating offer:", error);
+    throw new Error("Failed to update offer");
   }
 };
 
